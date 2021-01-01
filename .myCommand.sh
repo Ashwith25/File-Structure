01e8275d093fbadf0a09b88eb71529a4ef91c1f2
Ashwith Poojary <pashwith25@gmail.com>
2021-01-01 18:36:45 +0530

    Update .myCommand.sh

--- .myCommand.sh
+++ .myCommand.sh
@@ -1,5 +1,6 @@
 #!/bin/bash#
 
+#to create your own custom command, change the name of function
 function fileStructureOf() {
     cd
     python3 /home/ashwith/MyProjects/File-Structure/fileStructure.py $1
