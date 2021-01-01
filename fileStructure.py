b9af5b928d69fe18feb41f48d028e54cd8cc16aa
Ashwith <pashwith25@gmail.com>
2021-01-01 18:27:21 +0530

    version 1

--- fileStructure.py
+++ fileStructure.py
@@ -6,4 +6,26 @@ path = sys.argv[1]
 print(path)
 
 os.chdir(path)
-[print(files) for files in os.listdir()]
+File = []
+count = 0
+
+def fileNames(filepath):
+    global count
+    for files in sorted(os.listdir(filepath)):
+        if not os.path.isdir(os.path.join(filepath, files)):
+            print(" "*count*2,"\033[1;37;40m |-> "+f"\033[1;37;40m{files}")
+        if os.path.isdir(os.path.join(filepath, files)):
+            print(" "*count*2,"\033[1;37;40m |-"+f"\033[1;32;40m{files}")
+            count=count+1
+            fileNames(os.path.join(filepath, files))
+        else:
+            File.append(files)
+    count=count-1
+
+for files in sorted(os.listdir()):
+    newPath = os.path.join(path, files)
+    if not os.path.isdir(newPath):
+        print("\033[1;37;40m|-"+files)
+    if os.path.isdir(newPath):
+        print("\033[1;32;40m|-"+files)
+        fileNames(newPath)
